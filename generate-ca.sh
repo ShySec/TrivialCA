36aa2be97b0ad48e40637c6ae6646662b4f8a8d5
kelson <kelson@shysecurity.com>
2019-04-20 15:53:35 -0400

    - added simple v3_extensions support with DNS copy-in

--- generate-ca.sh
+++ generate-ca.sh
@@ -13,6 +13,7 @@ CA_PATH="ca/$CA_NAME"
 CA_CRT="$CA_PATH/$CA_NAME.crt"
 CA_KEY="$CA_PATH/$CA_NAME.key"
 CA_SRL="$CA_PATH/$CA_NAME.srl"
+CA_EXT="$CA_PATH/$CA_NAME.v3ext"
 
 SUBJECTDATA="/O=$CA_NAME"
 
@@ -28,3 +29,10 @@ openssl req -x509 -new -nodes -key "$CA_KEY" -sha256 -days "$DAYS" -out "$CA_CRT
 
 # Initialize the CA serial number
 echo "01" > "$CA_SRL"
+
+# Initialize the v3 extensions file
+cat <<EOF > "$CA_EXT"
+basicConstraints=CA:FALSE
+authorityKeyIdentifier=keyid,issuer
+keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
+EOF
--- sign-request.sh
+++ sign-request.sh
@@ -24,25 +24,36 @@ CA_PATH="ca/$CA_NAME"
 CA_CRT="$CA_PATH/$CA_NAME.crt"
 CA_KEY="$CA_PATH/$CA_NAME.key"
 CA_SRL="$CA_PATH/$CA_NAME.srl"
+CA_EXT="$CA_PATH/$CA_NAME.v3ext"
 
 SERVER_PATH="servers/$SERVER_NAME/$DATED"
 SERVER_KEY="$SERVER_PATH/$SERVER_NAME.key"
 SERVER_CRT="$SERVER_PATH/$SERVER_NAME.crt"
 SERVER_CSR="$SERVER_PATH/$SERVER_NAME.csr"
+SERVER_EXT="$CA_EXT"
 
 if [ ! -d "$SERVER_PATH" ]; then
 	mkdir -p "$SERVER_PATH"
 fi
 
-echo
-echo "double-check CSR canonical names and Subject Alternative Names"
-openssl req -noout -text -in "$SERVER_CSR" | grep --color=never "Subject:"
-echo
-
 # copy the SCR to our output directory for easy reference
 cp "$CSR_NAME" "$SERVER_CSR"
 
+echo
+echo "double-check CSR canonical names and Subject Alternative Names"
+openssl req -noout -text -in "$SERVER_CSR" | grep --color=never "Subject:\|DNS:"
+echo
+
+# copy the SAN DNS entries to v3.extensions
+SERVER_SAN=$(openssl req -noout -text -in "$SERVER_CSR" | grep --color=never "DNS:")
+
+if [ "$SERVER_SAN" != "" ]; then
+	SERVER_EXT="$SERVER_PATH/$SERVER_NAME.v3ext"
+	cp "$CA_EXT" "$SERVER_EXT"
+	echo "subjectAltName=$SERVER_SAN" >> "$SERVER_EXT"
+fi
+
 # Sign the server key with the certificate authority
-openssl x509 -req -in "$SERVER_CSR" -CA "$CA_CRT" -CAkey "$CA_KEY" -CAserial "$CA_SRL" -days "$DAYS" -out "$SERVER_CRT"
+openssl x509 -req -in "$SERVER_CSR" -extfile "$SERVER_EXT" -CA "$CA_CRT" -CAkey "$CA_KEY" -CAserial "$CA_SRL" -days "$DAYS" -out "$SERVER_CRT"
 
 echo "server credentials stored in $SERVER_PATH"
